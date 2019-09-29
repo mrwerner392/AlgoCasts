e1e23cef6647d133c9b07109e6032542707813f6
mrwerner392 <mrwerner392@gmail.com>
2019-09-28 22:49:52 -0400

    completed 'reversestring'

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,44 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+  let chars = str.split("");
+  let reversedChars = [];
+  for (let i = chars.length - 1; i >= 0; i--) {
+    reversedChars.push(chars[i]);
+
+  };
+  return reversedChars.join("");
+};
+
 
 module.exports = reverse;
+
+// // Video solution #1
+// function reverse(str) {
+//   return str.split("")
+//             .reverse()
+//             .join("");
+// };
+
+// // Video solution #2
+// function reverse(str) {
+//   let reversed = '';
+//   for (let character of str) {
+//     reversed = character + reversed;
+//   };
+//   return reversed;
+// };
+
+// // Video solution #3
+// function reverse(str) {
+//   return str.split("").reduce((rev, char) => char + rev, "");
+// };
+
+// function reverse(str) {
+//   let reversed = '';
+//   for (let i = str.length - 1; i >= 0; i--) {
+//     reversed += str[i];
+//   };
+//   return reversed;
+// }
