2dbe5962b9ba3bee886f2b360f3d45987c78aa5c
mrwerner392 <mrwerner392@gmail.com>
2019-10-01 19:54:45 -0400

    completed 'anagrams'

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,82 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+
+// My solution, but doesn't account for non-letters/upper and lower case
+function anagrams(stringA, stringB) {
+  const charsA = {};
+  const charsB = {};
+  let numCharsA = 0;
+  let numCharsB = 0;
+
+  for (let char of stringA) {
+    if (charsA[char]) {
+      charsA[char]++;
+    } else {
+      charsA[char] = 1;
+      numCharsA++;
+    };
+  };
+
+  for (let char of stringB) {
+    if (charsB[char]) {
+      charsB[char]++;
+    } else {
+      charsB[char] = 1;
+      numCharsB++;
+    };
+  };
+
+  let checker = true;
+
+  if (numCharsA !== numCharsB) {
+    return false;
+  };
+
+  for (let key in charsA) {
+    if (charsA[key] !== charsB[key]) {
+      return false;
+    };
+  };
+
+  return true;
+
+};
 
 module.exports = anagrams;
+
+
+// // Video solution #1
+// function anagrams(stringA, stringB) {
+//   const aCharMap = buildCharMap(stringA);
+//   const bCharMap = buildCharMap(stringB);
+//
+//   if (Object.keys(aCharMap).length !== Object.keys(bCharMap).length) {
+//     return false;
+//   };
+//
+//   for (let char in aCharMap) {
+//     if (aCharMap[char] !== bCharMap[char]) {
+//       return false;
+//     };
+//   };
+//
+//   return true;
+// };
+//
+// function buildCharMap(str) {
+//   const charMap = {};
+//   for (let char of str.replace(/[^\w]/g, '').toLowerCase()) {
+//     charMap[char] = charMap[char] + 1 || 1;
+//   };
+//   return charMap;
+// };
+
+// // Video solution #2
+// function anagrams(stringA, stringB) {
+//   return cleanString(stringA) === cleanString(stringB);
+// };
+//
+// function cleanString(str) {
+//   return str.replace(/[^\w]/g, '').toLowerCase().split('').sort().join('');
+// };
