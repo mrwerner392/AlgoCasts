dacf7bc30f81ead9efd79398ba307a70c4cb59e6
mrwerner392 <mrwerner392@gmail.com>
2019-10-05 11:56:29 -0400

    completed 'capitalize'

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -7,6 +7,41 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-function capitalize(str) {}
+
+// My solution
+function capitalize(str) {
+  let capWords = [];
+  let words = str.split(' ');
+  for (let word of words) {
+    capWords.push(word[0].toUpperCase() + word.slice(1));
+  };
+  return capWords.join(' ');
+};
 
 module.exports = capitalize;
+
+
+// // Video solution #1
+// function capitalize(str) {
+//   let words = [];
+//
+//   for (let word of str.split(' ')) {
+//     words.push(word[0].toUpperCase() + word.slice(1));
+//   };
+//
+//   return words.join(' ');
+// };
+
+
+// // Video solution #2
+// function capitalize(str) {
+//   let result = str[0].toUpperCase();
+//   for (let i = 1; i < str.length; i++) {
+//     if (str[i - 1] === ' ') {
+//       result += str[i].toUpperCase();
+//     } else {
+//       result += str[i];
+//     };
+//   };
+//   return result;
+// };
