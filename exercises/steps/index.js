ca2ff8394c51d60c012b422eab6cc7d86bf0e00a
mrwerner392 <mrwerner392@gmail.com>
2019-10-05 17:14:21 -0400

    completed 'steps'

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,6 +17,56 @@
 //       '### '
 //       '####'
 
-function steps(n) {}
+
+// My solution
+function steps(n) {
+  let str = '';
+  for (let i = 1; i <= n; i++) {
+    str += ' ';
+  };
+
+  for (let i = 0; i < n; i++) {
+    str = '#' + str.slice(0, n-1);
+    console.log(str);
+  };
+};
 
 module.exports = steps;
+
+
+// // Video solution #1
+// function steps(n) {
+//   for (let rowNum = 0; rowNum < n; rowNum++) {
+//     let str = ''
+//     for (let colNum = 0; colNum < n; colNum++) {
+//       if (colNum <= rowNum) {
+//         str += '#';
+//       } else {
+//         str += ' ';
+//       };
+//     };
+//     console.log(str);
+//   };
+// };
+
+// // Video solution #2
+// function steps(n, row = 0, str = '') {
+//   if (n === row) {
+//     return;
+//   };
+//
+//   if (n === str.length) {
+//     console.log(str);
+//     steps(n, row + 1);
+//     return
+//   };
+//
+//   if (str.length <= row) {
+//     str += '#';
+//   } else {
+//     str += ' ';
+//   };
+//
+//   steps(n, row, str);
+//
+// }
