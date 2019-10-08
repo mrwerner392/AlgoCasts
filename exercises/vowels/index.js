fbfe741b4ea8c32fa822400655cf2b5719f8dbfe
mrwerner392 <mrwerner392@gmail.com>
2019-10-08 19:21:47 -0400

    completed 'vowels'

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -7,6 +7,36 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+// My solution, recursion probably unnecessary but practicing while its fresh in mind
+function vowels(str, count = 0, index = 0) {
+  if (index === str.length) {
+    return count;
+  }
+
+  if (/[aeiouAEIOU]/.test(str[index])) {
+    count++;
+  };
+
+  return vowels(str, count, ++index);
+}
 
 module.exports = vowels;
+
+
+// // Video solution #1
+// function vowels(str) {
+//   let count = 0;
+//   const checker = ['a', 'e', 'i', 'o', 'u'];
+//   for (let char of str) {
+//     if (checker.includes(char.toLowerCase())) {
+//       count++
+//     };
+//   };
+//   return count;
+// };
+
+// // Video solution #2
+// function vowels(str) {
+//   let matches = str.match(/[aeiou]/gi);
+//   return matches ? matches.length : 0;
+// }
