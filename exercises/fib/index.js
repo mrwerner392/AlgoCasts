a808423417ede157bc58b259d1c1d2a58d8f7376
mrwerner392 <mrwerner392@gmail.com>
2019-10-13 13:12:51 -0400

    completed 'fib'

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -8,6 +8,76 @@
 // Example:
 //   fib(4) === 3
 
-function fib(n) {}
+// My solution
+// function fib(n) {
+//
+//   if (n === 0) {
+//     // console.log(0);
+//     return 0;
+//   } else if (n === 1) {
+//     // console.log(1);
+//     return 1;
+//   } else {
+//     const fib = [0, 1];
+//
+//     for (let i = 2; i <= n; i++) {
+//       fib[i] = fib[i - 1] + fib[i - 2];
+//     };
+//
+//     // console.log(fib[n]);
+//     return fib[n];
+//   };
+//
+// };
+
+// module.exports = fib;
+
+
+// // Video solution #1
+// function fib(n) {
+//
+//   const results = [0, 1];
+//
+//   for (let i = 2; i <= n; i++) {
+//     results[i] = results[i - 1] + results[i - 2];
+//   };
+//
+//   return results[n];
+//
+// };
+
+// // Video solution #2
+// function fib(n) {
+//   if (n < 2) {
+//     return n;
+//   }
+//
+//   return fib(n - 1) + fib(n - 2);
+// };
+
+// Video solution #3 memoization
+function memoize(fn) {
+  const cache = {};
+  return function(...args) {
+    if (cache[args]) {
+      return cache[args];
+    };
+
+    const result = fn.apply(this, args);
+    cache[args] = result;
+    return result;
+  };
+};
+
+function fib(n) {
+  if (n < 2) {
+    return n;
+  }
+
+  return fib(n - 1) + fib(n - 2);
+};
+
+
+fib = memoize(fib);
 
 module.exports = fib;
