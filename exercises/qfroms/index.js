935b333a3d8394103f0d67824fe28b4fb76a96dc
mrwerner392 <mrwerner392@gmail.com>
2019-10-16 19:27:48 -0400

    completed 'qfroms'

--- exercises/qfroms/index.js
+++ exercises/qfroms/index.js
@@ -12,8 +12,43 @@
 //     q.remove(); // returns 1
 //     q.remove(); // returns 2
 
+
+// My solution -- matches video solution
 const Stack = require('./stack');
 
-class Queue {}
+class Queue {
+
+
+  constructor() {
+    this.stackOne = new Stack();
+    this.stackTwo = new Stack();
+  };
+
+  add(item) {
+    this.stackOne.push(item);
+  };
+
+  remove() {
+    while (this.stackOne.peek()) {
+      this.stackTwo.push(this.stackOne.pop());
+    };
+    const removedItem = this.stackTwo.pop();
+    while (this.stackTwo.peek()) {
+      this.stackOne.push(this.stackTwo.pop());
+    };
+    return removedItem;
+  };
+
+  peek() {
+    while (this.stackOne.peek()) {
+      this.stackTwo.push(this.stackOne.pop());
+    };
+    const peekedItem = this.stackTwo.peek();
+    while (this.stackTwo.peek()) {
+      this.stackOne.push(this.stackTwo.pop());
+    };
+    return peekedItem;
+  };
+};
 
 module.exports = Queue;
